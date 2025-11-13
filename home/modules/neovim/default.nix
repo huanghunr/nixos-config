b844f4dac4bc1919e7756623d2cff0cbdc95cefd
huanghunr <huanghunr@outlook.com>
2025-11-13 16:25:22 +0800

    111

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -16,7 +16,7 @@ let
   };
   # the path to nvim directory
   # to make this symlink work, we need to git clone this repo to your home directory.
-  configPath = "${config.home.homeDirectory}/nix-config/home/modules/neovim/nvim";
+  configPath = "/home/huanghunr/nixos-config/home/modules/neovim/nvim";
 in
 {
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink configPath;
@@ -73,7 +73,6 @@ in
     plugins = with pkgs.vimPlugins; [
       # search all the plugins using https://search.nixos.org/packages
       telescope-fzf-native-nvim
-
       nvim-treesitter.withAllGrammars
     ];
   };
--- home/modules/neovim/nvim/init.lua
+++ home/modules/neovim/nvim/init.lua
@@ -15,5 +15,6 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
+-- require("nvim-treesitter.install").prefer_git = true
 require "lazy_setup"
 require "polish"
