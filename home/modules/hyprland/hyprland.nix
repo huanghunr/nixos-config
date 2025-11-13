b302851b05c79a8d443ec7669b707a83faeb0e05
huanghunr <huanghunr@outlook.com>
2025-11-13 23:13:06 +0800

    hyprland plugins

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -17,8 +17,18 @@
         "$mod ALT, mouse:272, resizewindow"
         ];
     };
+    
     plugins = [
         hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprscrolling
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprtrails
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.waybar
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.mako
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.Hyprshot
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.swaybg
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.Hyprpicker
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.Wlogout
+        hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.swaylock
     ];
   };
 }
