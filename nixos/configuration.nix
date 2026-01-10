1cb4fd6ae793de0e207391090d4daab3d8aa40c9
huanghunr <huanghunr@outlook.com>
2026-01-10 12:57:25 +0800

    fcitx5 fix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -62,12 +62,14 @@
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-rime # alternatively, kdePackages.fcitx5-qt
       qt6Packages.fcitx5-chinese-addons # table input method support
       fcitx5-nord # a color theme
       fcitx5-mozc
       fcitx5-gtk
+      rime-zhwiki
     ];
   };
 
