9d818ee7c728db522261b16f68e19406e4277154
huanghunr <160948982+huanghunr@users.noreply.github.com>
2026-02-19 15:26:53 +0800

    home: make xdg setup reproducible for hyprland

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -2,6 +2,24 @@
 {
   xdg = {
     enable = true;
+
+    # Ensure user directories are reproducible instead of relying on old desktop bootstrap.
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      desktop = "$HOME/Desktop";
+      documents = "$HOME/Documents";
+      download = "$HOME/Downloads";
+      music = "$HOME/Music";
+      pictures = "$HOME/Pictures";
+      publicShare = "$HOME/Public";
+      templates = "$HOME/Templates";
+      videos = "$HOME/Videos";
+      extraConfig = {
+        XDG_SCREENSHOTS_DIR = "$HOME/Pictures/Screenshots";
+      };
+    };
+
     desktopEntries = {
       typora = {
         name = "typora";
@@ -15,28 +33,38 @@
         icon = "";
       };
     };
-    portal = {
-      xdgOpenUsePortal = true;
-      extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal ];
-      # config = {
-      #   common = {
-      #     default = [ "gtk" ];
-      #     "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-      #   };
-      #   hyprland = {
-      #     default = [ "hyprland" "gtk" ];
-      #     "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-      #     "org.freedesktop.impl.portal.OpenURI" = [ "gtk" ];
-      #   };
-      # };
-    };
-    # mimeApps = {
-    #   defaultApplications = {
-    #     "text/html" = [ "com.microsoft.Edge.desktop" ];
-    #     "x-scheme-handler/http"  = [ "firefox.desktop" ];
-    #     "x-scheme-handler/https" = [ "com.microsoft.Edge.desktop" ];
-    #   };
-    # };
-  };
 
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-hyprland
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal
+      ];
+      config = {
+        common = {
+          default = [ "gtk" ];
+          "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        };
+        hyprland = {
+          default = [ "hyprland" "gtk" ];
+          "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+          "org.freedesktop.impl.portal.OpenURI" = [ "gtk" ];
+          "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        };
+      };
+    };
+
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = [ "com.microsoft.Edge.desktop" ];
+        "x-scheme-handler/http" = [ "com.microsoft.Edge.desktop" ];
+        "x-scheme-handler/https" = [ "com.microsoft.Edge.desktop" ];
+        "x-scheme-handler/about" = [ "com.microsoft.Edge.desktop" ];
+        "x-scheme-handler/unknown" = [ "com.microsoft.Edge.desktop" ];
+      };
+    };
+  };
 }
