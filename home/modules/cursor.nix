9ed29f04b6bedaaa155887440aa0b4edb4ee14ce
huanghunr <huanghunr@outlook.com>
2026-02-20 21:14:45 +0800

    cursor theme Bibata-Original-Ice

--- home/modules/cursor.nix
+++ home/modules/cursor.nix
@@ -1,26 +1,44 @@
 { pkgs, ... }:
+
+let
+  getFrom =
+    {
+      url,
+      hash,
+      name,
+    }:
+    {
+      enable = true;
+      gtk.enable = true;
+      x11.enable = true;
+      name = name;
+      size = 24;
+      package = pkgs.runCommand "moveUp" { } ''
+        mkdir -p $out/share/icons
+        ln -s ${
+          pkgs.fetchzip {
+            url = url;
+            hash = hash;
+          }
+        } $out/share/icons/${name}
+      '';
+    };
+
+  Bibata-Modern-Ice = getFrom {
+    url = "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz";
+    hash = "sha256-SG/NQd3K9DHNr9o4m49LJH+UC/a1eROUjrAQDSn3TAU=";
+    name = "Bibata-Modern-Ice";
+  };
+
+  Bibata-Original-Ice = getFrom {
+    url = "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Original-Ice.tar.xz";
+    hash = "sha256-RdOXxVXQ1YMfFbP3bktNnOQBhHMgfjjGsp0bJVQZ39c=";
+    name = "Bibata-Original-Ice";
+  };
+
+in
 {
-  home.pointerCursor =
-    let
-      getFrom = url: hash: name: {
-        gtk.enable = true;
-        x11.enable = true;
-        name = name;
-        size = 48;
-        package = pkgs.runCommand "moveUp" { } ''
-          mkdir -p $out/share/icons
-          ln -s ${
-            pkgs.fetchzip {
-              url = url;
-              hash = hash;
-            }
-          } $out/share/icons/${name}
-        '';
-      };
-    in
-    getFrom "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz"
-      "sha256-SG/NQd3K9DHNr9o4m49LJH+UC/a1eROUjrAQDSn3TAU="
-      "Bibata-Modern-Ice";
+  home.pointerCursor = Bibata-Original-Ice;
 
   # DPI and Xresources
   xresources.properties = {
